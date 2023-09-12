ee7ac6ffa77d5bc110fc4be011023850fb71b16d
omarsrepo <mmoh0033@student.monash.edu>
2023-09-13 01:22:09 +0800

    Documentation almost complete

--- FloorGraph.py
+++ FloorGraph.py
@@ -1,3 +1,4 @@
+
 from dataclasses import dataclass
 import heapq
 import networkx as nx
@@ -16,9 +17,11 @@ class FloorGraph:
         Populates self.vertices with all the vertices of the graph
 
         :Input:
-            paths: list of paths and keys represented as a list of tuples
+        paths: list of paths and keys represented as a list of tuples
+
         :Output,return or post condition:
-            vertices and edges: unique and sorted list of vertices
+        None. It simply initializes a FloorGraph object with a list of vertices
+
 
         Time complexity: Best and worst case O(|E|)
         :Aux space complexity: Best and worst case O(|V| + |E|)
@@ -36,10 +39,16 @@ class FloorGraph:
         # Create a list of all the vertices and populate the edges list for each vertex
         # Complexity of O(E+E) = O(E) because get_vertices iterates through all the edges,
         # and we also iterate through the edges to populate the edge list for every vertex
+
         self.vertices: list = self.get_vertices(self.paths)
         for edge in self.paths:
             u, v, w = edge
             self.vertices[u].edges.append(Edge(u, v, w))
+
+        # Add the keys into the respective vertices
+        for key in self.keys:
+            key_vertex, time = key
+            self.vertices[key_vertex].key_time = time
 
     def climb(self, start: int, exits: list) -> tuple | None:
         """
@@ -47,9 +56,22 @@ class FloorGraph:
         to climb to the next floor. It computes the most optimal route needed to collect a key and then head to
         an exit.
 
-        :param start: Starting location (integer)
-        :param exits: Possible exit points (list of exits)
-        :return: tuple (Shortest time, Path)
+        :Input:
+        paths: Integer of the vertex id indicating which vertex/location on the floor to start at
+        exits: list of integers (vertex ids) of all possible exits
+
+        :Output,return or post condition:
+        Tuple (Shortest time, Path) to climb to the next floor.
+
+        :Time complexity:
+        ----------------------
+
+
+        :Aux space complexity:
+        ----------------------
+
+
+        :return: Tuple (Shortest time, Path) to climb to the next floor.
         """
         # Check if we can even get to the exits from the start
         distances_from_start = self.dijkstra(start)
@@ -143,6 +165,19 @@ class FloorGraph:
         return total_time, final_route
 
     def reset_state(self):
+        """
+        This function simply resets all the class variables of every vertex object that exits.
+
+        :Time complexity:
+        ----------------------
+        Best and worst case time complexity of O(|V|) where |V| is the total no.of vertices
+
+        :Aux space complexity:
+        ----------------------
+        Best and worst case is O(1)
+
+        :return: None
+        """
         for vertex in self.vertices:
             if vertex is not None:
                 vertex.distance = 0
@@ -150,74 +185,112 @@ class FloorGraph:
                 vertex.discovered = False
                 vertex.visited = False
 
-    def dijkstra(self, source: int):
+    def dijkstra(self, source: int) -> list:
         """
         Dijkstra's algorithm which is a modified bfs that accepts as input a vertex id which is where
         we start at and computes the shortest distance to every other vertex from the start/source.
 
-        :param source: Vertex
-        :return:
-        """
-        # Creating a list
-        memo = [None] * len(self.vertices)
-        start = self.vertices[source]
-        start.distance = 0
+        :Input:
+            paths: list of paths and keys represented as a list of tuples
+        :Output,return or post condition:
+            vertices and edges: unique and sorted list of vertices
 
+        :Time complexity:
+        ----------------------
+        Best case is O(|V|) when the graph is spare and has very few edges (1 edge per vertex)
+        Worst case is O(Section2+Section1) = O(|E|+|V|log|V|)
+
+        :Aux space complexity:
+        ----------------------
+        distances_from_start: This is an array of length |V|, where |V| is the number of vertices in the graph.
+                              It stores the shortest distances from the source vertex to all other vertices.
+                              The space complexity for this array is O(|V|).
+        discovered: This is a priority queue (implemented as a MinHeap) to keep track of vertices to be explored.
+                    The space complexity for the priority queue can be up to O(|V|) in the worst case.
+
+        Best and worst case Auxiliary space complexity is O(|V|) respectively where |V| is the no.of vertices
+
+        :return: List of distances from start to every other vertex
+        """
+        # Creating a list of distances from start vertex to all other vertexes to act as a look-up table.
+        current_vertex = None
+        distances_from_start = [None] * len(self.vertices)
+
+        start_location = self.vertices[source]
+        start_location.distance = 0
+
+        # Creating a priority queue to decide which discovered vertex to visit next
         discovered = MinHeap()
-        discovered.push(start.id)
-        start.discovered = True
+
+        discovered.push(start_location.id)
+        start_location.discovered = True
         i = 0
+
         while discovered.size() > 0:
-            # From the heap/array, pop the vertex, and now it is visited so update the visited
-            # Once the vertex has been visited, it
+            """ ---------------------------------Section 1-----------------------------------"""
+            # i is a counter which decided what we push into the heap. On the first iteration, we only push the start vertex id
+            # From the second iteration onwards, we push the distances of discovered vertices instead. When we pop from the heap
+            # we will always be choosing to visit the closest neighboring vertex, thus we choose a greedy approach
+            # This section has a time complexity of |V|*log|V|
             if i == 0:
-                u = self.vertices[discovered.pop()]
-                u.visited = True
+                current_vertex = self.vertices[discovered.pop()]
+                current_vertex.visited = True
             else:
                 u_distance = discovered.pop()
                 for vertex in self.vertices:
                     if vertex is not None:
                         if not vertex.visited:
                             if vertex.distance == u_distance:
-                                u = vertex
-                                u.visited = True
+                                current_vertex = vertex
+                                current_vertex.visited = True
                                 break
 
-            # Now we will look at all the edges that this vertex (u) has in its edges list
-            for edge in u.edges:
-                # print(f"distance of vertex{u.id} is {u.distance}")
+            """ Things to note """
+            # u = current vertex
+            # v = neighboring vertex
+
+            """ ---------------------------------Section 2-----------------------------------"""
+            # This section has a time complexity of O(|E|)
+            # Now we will look at all the edges that this vertex has in its edges list
+            for edge in current_vertex.edges:
                 v = self.vertices[edge.v]  # Look at the neighboring vertex
                 if not v.discovered:
                     v.discovered = True
-                    v.distance = u.distance + edge.time
-                    v.previous = u
-                    discovered.push(v.distance)
-                    memo[v.id] = v.distance
+                    v.distance = current_vertex.distance + edge.time
+                    v.previous = current_vertex
+                    discovered.push(v.distance)  # adding the newly discovered neighbouring vertex into the heap based on its distance
+                    distances_from_start[v.id] = v.distance  # Updating the distance to this neighbouring vertex from the start vertex
                 elif not v.visited:
-                    if v.distance > u.distance + edge.time:
+                    if v.distance > current_vertex.distance + edge.time:
                         # update distance
-                        v.distance = u.distance + edge.time
-                        v.previous = u
+                        v.distance = current_vertex.distance + edge.time
+                        v.previous = current_vertex
                         discovered.push(v.distance)
-                        memo[v.id] = v.distance
-
+                        distances_from_start[v.id] = v.distance
             i += 1
-        return memo
+        return distances_from_start
 
     def get_vertices(self, paths: list[tuple]) -> list:
         """
         This function will take in the list of tuples of paths in the graph
         and will return to us a unique list of all the vertices in ascending order
+
         :Input:
             paths: list of paths represented as a list of tuples(u,v,x)
         :Output,return or postcondition:
             vertices: unique and sorted list of vertices
-        Time complexity: Best and worst case O(E)
-        :Aux space complexity: Best and worst case O(V)
+
+        :Time complexity:
+        ----------------------
+        Best and worst case O(|E|+|V|) as we have to iterate through all edges and all vertices
+
+        :Aux space complexity:
+        ----------------------
+        Best and worst case is O(|V|) as we create a list of all vertices, thus the size is equal to total no.of vertices
         """
         # First we obtain the biggest vertex ID
         greatest_vertex = 0
-        for path in paths:  # Worst case time complexity of O(E)
+        for path in paths:  # Iterate through every edge, thus time complexity of O(|E|)
             u, v, x = path
             if u > greatest_vertex:
                 greatest_vertex = u
@@ -225,46 +298,9 @@ class FloorGraph:
                 greatest_vertex = v
         # Now we create a unique list of vertices in ascending order
         vertices = [None] * (greatest_vertex + 1)
-        for i in range(len(vertices)):  # Worst case time complexity of O(E)
-            # u, v, x = paths[i]
-            # vertices[u] = Vertex(u)
-            # vertices[v] = Vertex(v)
+        for i in range(len(vertices)):  # Iterate through every vertex, thus time complexity of O(|V|)
             vertices[i] = Vertex(i)
         return vertices
-
-    # Tester function
-    def draw_graph(self):
-        """
-        Visualize the graph using networkx and matplotlib.
-        """
-        G = nx.Graph()
-        # Add nodes
-        for vertex in self.vertices:
-            G.add_node(vertex.id)
-            for edge in vertex.edges:
-                G.add_edge(edge.u, edge.v, weight=edge.time)
-        # Add edges
-        # for edge in self.edges:
-        #     G.add_edge(edge.u, edge.v, weight=edge.time)
-        # Draw the graph
-        pos = nx.spring_layout(G)  # You can change the layout algorithm as needed
-        nx.draw(G, pos, with_labels=True, node_size=500, font_size=10, node_color='lightblue')
-        # Draw edges with arrows
-        vertex_edges = []
-        for vertex in self.vertices:
-            for edge in vertex.edges:
-                if edge not in vertex_edges:
-                    vertex_edges.append(edge)
-                    
-        edge_labels = {(edge.u, edge.v): edge.time for edge in vertex_edges}  # Optional edge labels
-        nx.draw_networkx_edges(G, pos, edgelist=list(G.edges()), connectionstyle="arc3, rad=0.2", arrowsize=20)
-        # Draw node labels
-        labels = {node: node for node in G.nodes()}
-        nx.draw_networkx_labels(G, pos, labels, font_size=10)
-        # Draw edge labels (optional)
-        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=10)
-        plt.title("Graph Visualization")
-        plt.show()  #
 
 
 @dataclass
@@ -272,8 +308,25 @@ class Vertex:
     id: int
 
     def __post_init__(self):
+        """
+        Class for creating a vertex object
+
+        :Input: None
+        :Output,return or post condition: None
+
+        :Time complexity:
+        ----------------------
+        Best and worst case of O(1)
+
+        :Aux space complexity:
+        ----------------------
+        Best and worst case of O(1)
+
+        :return: None
+        """
         self.edges = []  # This is the adjacency list as we can see every edge for this vertex
         self.distance = 0
+        self.key_time = None
 
         # for backtracking
         self.previous = None  # Previous vertex that it came from
@@ -285,32 +338,64 @@ class Vertex:
 
 @dataclass
 class Edge:
+    """
+    Class for creating Edge object.
+
+    :Input: None
+    :Output,return or post condition: None
+
+    :Time complexity:
+    ----------------------
+    Best and worst case of O(1)
+
+    :Aux space complexity:
+    ----------------------
+    Best and worst case of O(1)
+
+    :return: None
+    """
     u: Vertex
     v: Vertex
     time: int
 
 
 class MinHeap:
-    def __init__(self):
+    """
+    This is a class for a binary min-heap data structure.
+
+    :Input: None
+    :Output,return or post condition: None
+
+    :Time complexity:
+    ----------------------
+    Best case of O(1) for certain operations and worst case of O(log n) for certain operations
+
+    :Aux space complexity:
+    ----------------------
+    Best and worst case of O(1)
+
+    :return: None
+    """
+    def __init__(self):  # O(1)
         self.heap = []
 
-    def push(self, item):
+    def push(self, item):  # O(log n)
         heapq.heappush(self.heap, item)
 
-    def pop(self):
+    def pop(self):  # O(log n)
         if self.is_empty():
             raise IndexError("Heap is empty")
         return heapq.heappop(self.heap)
 
-    def peek(self):
+    def peek(self):  # O(1)
         if self.is_empty():
             raise IndexError("Heap is empty")
         return self.heap[0]
 
-    def is_empty(self):
+    def is_empty(self):  # O(1)
         return len(self.heap) == 0
 
-    def size(self):
+    def size(self):  # O(1)
         return len(self.heap)
 
     def print_heap(self):
@@ -332,6 +417,22 @@ class MinHeap:
 
 
 class Stack:
+    """
+    This is a class for a Stack ADT.
+
+    :Input: None
+    :Output,return or post condition: Stack object
+
+    :Time complexity:
+    ----------------------
+    Best and worst case of O(1) for all operations
+
+    :Aux space complexity:
+    ----------------------
+    Best and worst case of O(1)
+
+    :return: None
+    """
     def __init__(self):
         self.items = []
 
@@ -359,13 +460,53 @@ class Stack:
 
 if __name__ == "__main__":
     # The paths and keys represented as a list of tuples
-    paths = [(0, 1, 4), (0, 3, 2), (0, 2, 3), (2, 3, 2), (3, 0, 3)]
-    keys = [(0, 5), (3, 2), (1, 3)]
+    paths = [(0, 1, 4), (1, 2, 2), (2, 3, 3), (3, 4, 1), (1, 5, 2), (5, 6, 5), (6, 3, 2), (6, 4, 3), (1, 7, 4),
+             (7, 8, 2), (8, 7, 2), (7, 3, 2), (8, 0, 11), (4, 3, 1), (4, 8, 10)]
+    keys = [(5, 10), (6, 1), (7, 5), (0, 3), (8, 4)]
     graph = FloorGraph(paths, keys)
-    start = 3
-    exits = [3]
 
-    print(graph.vertices)
-    for vertex in graph.vertices:
-        if vertex is not None:
-            print(f"{vertex}: {vertex.edges}")
+    # print(graph.vertices)
+    # for vertex in graph.vertices:
+    #     if vertex is not None:
+    #         print(f"{vertex}: {vertex.edges}")
+    distance_from_start = graph.dijkstra(1)
+    print(f"distances_from_{1}: {distance_from_start}\n")
+
+    key_heap = MinHeap()
+    for key in keys:
+        key_heap.push(key[0])
+    # print(f"keys are at {key_heap.heap}\n")
+    # for key in key_heap.heap:
+    #     print(f"key at {key} takes {graph.vertices[key].key_time} mins")
+
+    total = float('inf')
+    key_to_go_for = None
+    while key_heap.size() > 0:
+        vertex_id = key_heap.pop()
+        if distance_from_start[vertex_id] is not None:
+            if total > distance_from_start[vertex_id] + graph.vertices[vertex_id].key_time:
+                total = distance_from_start[vertex_id] + graph.vertices[vertex_id].key_time
+                key_to_go_for = vertex_id
+
+    print(total, key_to_go_for)
+    """
+    Climb function logic:
+    ---------------------
+    climb(start: int, exits: list)
+    
+    start  = 1 --> Vertex(1)
+    distances_from_start = self.dijkstra(start) 
+    
+    distances_from_start = [17, None, 2, 5, 6, 2, 7, 4, 6]
+    total_time = float('inf')
+    final_route = []
+    
+    ------------------------------------ITERATE THROUGH EVERY KEY------------------------------------
+            REMEMBER THAT --> distances_from_start = [17, None, 2, 5, 6, 2, 7, 4, 6]
+    
+    for key in keys:  # keys = [(5, 10), (6, 1), (7, 5), (0, 3), (8, 4)]
+        vertex, time = key
+        if distances_from_start[vertex] is not None:
+            time = distances_from_start[vertex] + self.vertices.key_time  # Time to get to key and obtain it
+            
+    """
